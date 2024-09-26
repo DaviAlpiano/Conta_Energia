0e457a0bdbccae35839fbf4dc52e189994b67386
Davi Alpiano <davialpiano@gmail.com>
2024-09-26 11:50:10 -0300

    Inicio

--- src/main/java/com/betrybe/minhaconta/Main.java
+++ src/main/java/com/betrybe/minhaconta/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
 
   /**
-   * The entry point of application.
+   * The entry point of application. Inicio
    */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
